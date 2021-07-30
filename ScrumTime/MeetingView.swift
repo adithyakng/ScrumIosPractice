ecd95188a5c15e2af964161b77d0a3641cc0e1ac
Adithya KNG <adithyakng@gmail.com>
2021-07-30 20:45:29 +0530

    Added meeting View screen

---
+++ ScrumTime/MeetingView.swift
@@ -0,0 +1,52 @@
+//
+//  MeetingView.swift
+//  ScrumTime
+//
+//  Created by Adithya KNG on 30/07/21.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 10, total: 40)
+            HStack{
+                VStack(alignment:.leading){
+                    Text("Seconds Elasped")
+                        .font(.caption)
+                    Label("300", systemImage:"hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment:.leading){
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("500",systemImage: "hourglass.tophalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time Remaining")
+            .accessibilityValue("11 Minutes")
+            
+            Circle()
+                .strokeBorder(lineWidth: 24,antialiased: true)
+            HStack{
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: ({})){
+                    Image(systemName: "forward.fill")
+                        .foregroundColor(.black)
+                }
+            }
+            .accessibilityLabel("Next Speaker")
+            
+        }
+        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
